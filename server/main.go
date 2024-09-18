9637c7a40b48058fe9c308715e5275eaeedcafdf
Zaid Mouhamad <zmouhamad@MacBook-Pro-de-Zaid.local>
2024-09-18 17:21:54 +0200

    install dependencies air

--- server/main.go
+++ server/main.go
@@ -13,7 +13,7 @@ func main() {
 	// Define a route for the GET method on the root path '/'
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
-		return c.SendString("Hello, World !")
+		return c.SendString("Hello, World !!")
 	})
 
 	// Start the server on port 3000
