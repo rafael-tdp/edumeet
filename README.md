b3eeb3f48d1cf3406dfb2e469ef20bcfb137d87a
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2024-10-01 22:53:08 +0200

    mise en place de l'orm ent + creation schema user

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# EduMeet coucou
+# EduMeet
+
+## Lancer le backend
+
+- Mode normal : 
+
+```sh
+go run .
+```
+
+- Executer les migrations BDD : 
+
+```sh
+go run . mode=migrate
+```
+
+- Executer les fixtures 
+
+```sh
+go run . mode=fixture
+```
--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
@@ -10,16 +11,24 @@ import (
 )
 
 func main() {
-	// Initialize a new Fiber app
-	app := fiber.New()
-	//name := lo.Uniq([]string{"Samuel", "John", "Samuel"})
-	// Define a route for the GET method on the root path '/'
-	app.Get("/", func(c fiber.Ctx) error {
-		// Send a string response to the client
-		fmt.Println(ulid.Make())
-		return c.SendString("Hello, World !" + gofakeit.Email())
-	})
+	mode := flag.String("mode", "normal", "Choose the mode: normal, fixture or migrate")
+	flag.Parse()
+	if *mode == "migrate" {
+		migrate()
+	} else if *mode == "fixture" {
+		//fixture()
+	} else {
+		// Initialize a new Fiber app
+		app := fiber.New()
+		//name := lo.Uniq([]string{"Samuel", "John", "Samuel"})
+		// Define a route for the GET method on the root path '/'
+		app.Get("/", func(c fiber.Ctx) error {
+			// Send a string response to the client
+			fmt.Println(ulid.Make())
+			return c.SendString("Hello, World !" + gofakeit.Email())
+		})
 
-	// Start the server on port 3000
-	log.Fatal(app.Listen(":3000"))
+		// Start the server on port 3000
+		log.Fatal(app.Listen(":3000"))
+	}
 }
