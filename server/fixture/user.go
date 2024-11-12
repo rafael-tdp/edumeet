81792a8ec846080046c123532a9a0a4b9a07d5c9
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2024-11-12 21:45:53 +0100

    add real address to user fixture

--- server/fixture/user.go
+++ server/fixture/user.go
@@ -12,10 +12,15 @@ import (
 type User struct{}
 
 func (u *User) GenerateUser(ctx context.Context, client *ent.Client) {
-	physicalUser := []string{"Zaid", "Jugurtha", "Rapahel", "Makan"}
+	physicalUser := []string{"Zaid", "Jugurtha", "Rafael", "Makan"}
+	physicalAddress := []string{"242 Rue du Faubourg Saint-Antoine, 75012 Paris", "105 Stoke Newington Church St, London N16 0UD, Royaume-Uni", "44 Rue des Clottins, 95560 Montsoult", "14 Rue Édouard-Grimaux, 86000 Poitiers"}
 	dateUtils := utils.Date{}
 	bcryptUtils := utils.Bcrypt{}
 	for i := 0; i < len(physicalUser); i++ {
+		lat, lng, err := utils.GetLatLng(physicalAddress[i])
+		if err != nil {
+			panic(err)
+		}
 		client.User.Create().
 			SetEmail(strings.ToLower(physicalUser[i]) + "@user.com").
 			SetUsername(strings.ToLower(physicalUser[i])).
@@ -25,8 +30,8 @@ func (u *User) GenerateUser(ctx context.Context, client *ent.Client) {
 			SetBirthDate(dateUtils.GenerateBirthDate()).
 			SetBio(gofakeit.Sentence(10)).
 			SetActivated(true).
-			SetLng(gofakeit.Longitude()).
-			SetLat(gofakeit.Latitude()).
+			SetLng(lng).
+			SetLat(lat).
 			SaveX(ctx)
 	}
 }
