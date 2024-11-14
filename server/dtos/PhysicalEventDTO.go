0fbc1dafdea6e2948657aa62cf213d504c331e0f
Jugurtha Zekhnine <jugzek92@outlook.fr>
2024-11-14 14:34:17 +0100

    add id in dtos

--- server/dtos/PhysicalEventDTO.go
+++ server/dtos/PhysicalEventDTO.go
@@ -3,6 +3,7 @@ package dtos
 import "edumeet/ent"
 
 type PhysicalEventDTO struct {
+	ID       string  `json:"id"`
 	Location string  `json:"location"`
 	Lng      float64 `json:"lng"`
 	Lat      float64 `json:"lat"`
@@ -10,6 +11,7 @@ type PhysicalEventDTO struct {
 
 func EntToPhysicalEventDTO(physicalEvent *ent.PhysicalEvent) *PhysicalEventDTO {
 	return &PhysicalEventDTO{
+		ID:       physicalEvent.ID,
 		Location: physicalEvent.Location,
 		Lng:      physicalEvent.Lng,
 		Lat:      physicalEvent.Lat,
--- server/dtos/RemoteEventDTO.go
+++ server/dtos/RemoteEventDTO.go
@@ -3,11 +3,13 @@ package dtos
 import "edumeet/ent"
 
 type RemoteEventDTO struct {
+	ID  string `json:"id"`
 	URL string `json:"url"`
 }
 
 func EntToRemoteEventDTO(remoteEvent *ent.RemoteEvent) *RemoteEventDTO {
 	return &RemoteEventDTO{
+		ID:  remoteEvent.ID,
 		URL: remoteEvent.URL,
 	}
 }
