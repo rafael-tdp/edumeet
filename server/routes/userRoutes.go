e85cd085313e4671c38dec8dd1fbb9767aa352a4
mkamissoko <55959849+Mkamissoko@users.noreply.github.com>
2024-11-20 21:28:50 +0100

    Ajout d'une route reset password

--- server/routes/userRoutes.go
+++ server/routes/userRoutes.go
@@ -15,12 +15,13 @@ func setupRoutesUser(app *fiber.App, userController *controllers.UserController)
 	app.Get("/user/:id", middlewares.JWTAuthMiddleware, userController.GetUser)
 	app.Post("/user/register", userController.Register)
 	app.Post("/user/validate-user", userController.ValidateUser)
+	app.Post("/user/verify", userController.Verify)
 	app.Get("/user/resend-verify-email/:email", userController.ResendEmailValidateUser)
 	app.Post("/login", userController.Login)
 	app.Get("/me", middlewares.JWTAuthMiddleware, userController.Me)
 	app.Post("/user/forgot-password", userController.ForgotPassword)
-	app.Get("/user/verify/:code", userController.Verify)
-	app.Post("/user/reset-password/:code", userController.ResetPassword)
+	app.Post("/user/reset-password", userController.ResetPassword)
+
 }
 
 func initUserController(client *ent.Client) *controllers.UserController {
