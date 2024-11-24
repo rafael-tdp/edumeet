ba658a36611390528fe72863099481e0a3c54fd5
mkamissoko <55959849+Mkamissoko@users.noreply.github.com>
2024-11-24 23:35:47 +0100

    Suppression des méthodes inutiles

--- client/lib/core/models/response.dart
+++ client/lib/core/models/response.dart
@@ -1,9 +1,5 @@
 import 'dart:convert';
 
-ResponseRequest responseRequestFromJson(String str) => ResponseRequest.fromJson(json.decode(str));
-
-String responseRequestToJson(ResponseRequest data) => json.encode(data.toJson());
-
 class ResponseRequest {
   final bool success;
   final String? message;
@@ -16,18 +12,4 @@ class ResponseRequest {
     this.errors,
     this.data,
   });
-
-  factory ResponseRequest.fromJson(Map<String, dynamic> json) =>
-      ResponseRequest(
-        success: json["success"],
-        message: json["message"],
-        errors: json["errors"],
-        data: json["data"],
-      );
-
-  Map<String, dynamic> toJson() => {
-    "message": message,
-    "errors": errors,
-    "data": data,
-  };
 }
