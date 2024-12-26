6c45b1f8c8defee4b1be19d516b74a3e28050848
jugo92 <jugzek92@outlook.fr>
2024-12-26 15:36:13 +0100

    add participant when create event

--- server/services/EventService.go
+++ server/services/EventService.go
@@ -45,6 +45,12 @@ func (es *EventService) CreateEvent(ctx context.Context, eventDTO dtos.EventDTO,
 		}
 	}
 
+	_, err = es.participantRepository.CreateParticipant(userId, event.ID, "ACCEPTED")
+
+	if err != nil {
+		return nil, err
+	}
+
 	eventCreatedWithEdge, err := es.eventRepository.GetEvent(event.ID)
 	if err != nil {
 		return nil, err
