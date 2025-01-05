bc12b0067bced84accdf66d281bd7afc783c62bc
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2025-01-05 19:18:50 +0100

    ajout des markers

--- client/lib/screens/events_maps_screen.dart
+++ client/lib/screens/events_maps_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 import 'package:geocoding/geocoding.dart' as geocoding;
+import 'package:url_launcher/url_launcher.dart';
 import '../core/services/event_services.dart';
+import 'package:client/utils/date_utils.dart' as d;
 
 class EventsMapsScreen extends StatefulWidget {
   const EventsMapsScreen({super.key});
@@ -15,11 +18,15 @@ class EventsMapsScreen extends StatefulWidget {
 class _EventsMapsScreenState extends State<EventsMapsScreen> {
   late GoogleMapController mapController;
   Location location = Location();
+  MapType _currentMapType = MapType.normal;
   Set<Marker> markers = {};
+  late LocationData _userLocation;
+  bool _isLoading = true;
+  bool _hasError = false;
 
   Future<LatLng> _getUserLocation() async {
-    final userLocation = await location.getLocation();
-    return LatLng(userLocation.latitude!, userLocation.longitude!);
+    _userLocation = await location.getLocation();
+    return LatLng(_userLocation.latitude!, _userLocation.longitude!);
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -28,63 +35,71 @@ class _EventsMapsScreenState extends State<EventsMapsScreen> {
 
   Future<void> _loadEvents() async {
     try {
-      final userLocation = await _getUserLocation();
-      final events = await EventServices.getEvents([], userLocation.latitude, userLocation.longitude, 'physical', 50);
+      final events = await EventServices.getEvents([], _userLocation.latitude, _userLocation.longitude, 'physical', null);
       for (var event in events) {
         if (event.physicalEvent != null) {
           final eventLng = event.physicalEvent['lng'];
           final eventLat = event.physicalEvent['lat'];
           if (eventLat != null && eventLng != null) {
             final LatLng eventPosition = LatLng(eventLat, eventLng);
+            final eventDate = d.DateUtils.isoToFormattedDateAndTime(event.startDate);
             markers.add(
               Marker(
                 markerId: MarkerId(event.id!),
                 position: eventPosition,
-                infoWindow: InfoWindow(title: event.title, snippet: event.description),
+                infoWindow: InfoWindow(title: event.title, snippet: eventDate),
               ),
             );
           }
         }
       }
-      setState(() {});
+      setState(() {
+        _isLoading = false;
+      });
     } catch (e) {
       print('Error loading events: $e');
+      setState(() {
+        _isLoading = false;
+        _hasError = true;
+      });
     }
   }
 
   @override
   void initState() {
     super.initState();
-    _loadEvents();
+    _getUserLocation().then((_) => _loadEvents());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<LatLng>(
-        future: _getUserLocation(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Error retrieving location'));
-          } else {
-            LatLng _center = snapshot.data!;
-            return GoogleMap(
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    } else if (_hasError) {
+      return const Center(child: Text('Error retrieving location'));
+    } else {
+      LatLng _center = LatLng(_userLocation.latitude!, _userLocation.longitude!);
+      return Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
               onMapCreated: _onMapCreated,
               myLocationButtonEnabled: true,
               myLocationEnabled: true,
               scrollGesturesEnabled: true,
               zoomGesturesEnabled: true,
+              tiltGesturesEnabled: true,
+              rotateGesturesEnabled: true,
               markers: markers,
+              mapType: _currentMapType,
               initialCameraPosition: CameraPosition(
                 target: _center,
                 zoom: 15,
               ),
-            );
-          }
-        },
-      ),
-    );
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
